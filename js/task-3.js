4b56c8d24b2b25f5105020e58a0dad85b70bc9fe
olenka <elena.kaminnik@gmail.com>
2024-07-25 16:38:33 +0300

    add task file

--- js/task-3.js
+++ js/task-3.js
@@ -1,4 +1,12 @@
-function filterArray(numbers, value) {}
+function filterArray(numbers, value) {
+  const result = [];
+  for (const number of numbers) {
+    if (number > value) {
+      result.push(number);
+    }
+  }
+  return result;
+}
 
 console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
 console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
